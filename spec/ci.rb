bab0fcdc8b93af8f1a9172e0e987cc36e85ceaac
Sergio Cambra <sergio@programatica.es>
2016-04-15 15:40:44 +0200

    corregir travis

--- spec/ci.rb
+++ spec/ci.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 
-exit system('cd spec/dummy && bundle install --without debug && bundle exec rake db:create && bundle exec rake db:migrate && cd ../../ && BUNDLE_GEMFILE=spec/dummy/$BUNDLE_GEMFILE bundle exec rspec spec')
+exit system('cd spec/dummy && export BUNDLE_GEMFILE=../../$BUNDLE_GEMFILE && bundle install --without debug && bundle exec rake db:create && bundle exec rake db:migrate && cd ../../ && BUNDLE_GEMFILE=spec/dummy/$BUNDLE_GEMFILE bundle exec rspec spec')
