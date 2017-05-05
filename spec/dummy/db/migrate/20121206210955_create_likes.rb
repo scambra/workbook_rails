97c4b7c570bc84876f7aa75389e5b46881bb1b80
Sergio Cambra <sergio@programatica.es>
2017-05-05 14:34:02 +0200

    fix tests against rails < 5.1

--- spec/dummy/db/migrate/20121206210955_create_likes.rb
+++ spec/dummy/db/migrate/20121206210955_create_likes.rb
@@ -1,4 +1,4 @@
-class CreateLikes < ActiveRecord::Migration[5.0]
+class CreateLikes < (Rails.version < '5.1' ? ActiveRecord::Migration : ActiveRecord::Migration[5.0])
   def change
     create_table :likes do |t|
       t.string :name
